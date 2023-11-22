d27de0b993013809a35f2d4e23edcde3689ea748
Luis Manyari <lmanyari@hotmail.com>
2023-11-21 20:42:56 -0500

    :P~ first commit!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+// import { useState } from 'react';
 
-function App() {
+// export default function InputComponent() { 
+//   const [inputText, setText] = useState('hello'); 
+
+//   function handleChange(e) { 
+//     setText(e.target.value); 
+//   } 
+
+//   return ( 
+//     <> 
+//       <input value={inputText} onChange={handleChange} /> 
+//       <p>You typed: {inputText}</p> 
+//       <button onClick={() => setText('hello')}> 
+//         Reset 
+//       </button> 
+//     </> 
+//   ); 
+// } 
+
+
+// import { useState } from 'react'; 
+
+// export default function RegisterForm() { 
+//   const [form, setForm] = useState({ 
+//     firstName: 'Luke', 
+//     lastName: 'Jones', 
+//     email: 'lukeJones@sculpture.com', 
+//   }); 
+
+//   return ( 
+//     <> 
+//       <label> 
+//         First name: 
+//         <input 
+//           value={form.firstName} 
+//           onChange={e => { 
+//             setForm({ 
+//               ...form, 
+//               firstName: e.target.value 
+//             }); 
+//           }} 
+//         /> 
+//       </label> 
+//       <label> 
+//         Last name: 
+//         <input 
+//           value={form.lastName} 
+//           onChange={e => { 
+//             setForm({ 
+//               ...form, 
+//               lastName: e.target.value 
+//             }); 
+//           }} 
+//         /> 
+//       </label> 
+//       <label> 
+//         Email: 
+//         <input 
+//           value={form.email} 
+//           onChange={e => { 
+//             setForm({ 
+//               ...form, 
+//               email: e.target.value 
+//             }); 
+//           }} 
+//         /> 
+//       </label> 
+//       <p> 
+//         {form.firstName}{' '} 
+//         {form.lastName}{' '} 
+//         ({form.email})
+//       </p> 
+//     </> 
+//   ); 
+// } 
+
+import { useRef } from 'react'; 
+
+
+function TextInputWithFocusButton() {
+  const inputEl = useRef(null);
+  const onButtonClick = () => {
+    // `current` points to the mounted text input element
+    inputEl.current.focus();
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input ref={inputEl} type="text" />
+      <button onClick={onButtonClick}>Focus the input</button>
+    </>
   );
 }
 
-export default App;
+export default TextInputWithFocusButton;
